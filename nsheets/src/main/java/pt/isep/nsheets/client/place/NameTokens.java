9a64835fda6b9d7bafdbc6c058373e184ba9f697
Pedro Monteiro - 1160630 <1160630@isep.ipp.pt>
2018-05-30 14:18:46 +0100

    Added Chart option to menu bar;

--- nsheets/src/main/java/pt/isep/nsheets/client/place/NameTokens.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/place/NameTokens.java
@@ -27,6 +27,8 @@ public class NameTokens {
 
 	public static final String workbook = "workbook";
         
+        public static final String chart = "chart";
+        
         public static final String login = "login";
 
 	public static String getWorkbook() {
@@ -44,5 +46,9 @@ public class NameTokens {
 	public static String getLogin() {
 		return login;
 	}
+        
+        public static String getChart(){
+            return chart;
+        }
 
 }
