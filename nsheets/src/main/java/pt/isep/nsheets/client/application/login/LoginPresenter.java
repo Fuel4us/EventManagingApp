88224024947d516849cc54e87aaf2af8ffdf2be5
1160557 <1160557@isep.ipp.pt>
2018-05-30 17:14:29 +0100

    Login UI Login Button Handler

--- nsheets/src/main/java/pt/isep/nsheets/client/application/login/LoginPresenter.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/login/LoginPresenter.java
@@ -1,6 +1,8 @@
 package pt.isep.nsheets.client.application.login;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
@@ -10,18 +12,23 @@ import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 
 
 import com.gwtplatform.mvp.client.annotations.NameToken;
+import gwt.material.design.client.ui.MaterialToast;
 import pt.isep.nsheets.client.application.ApplicationPresenter;
 import pt.isep.nsheets.client.event.SetPageTitleEvent;
 import pt.isep.nsheets.client.place.NameTokens;
+import pt.isep.nsheets.shared.services.WorkbookDescriptionDTO;
+import pt.isep.nsheets.shared.services.WorkbooksService;
+import pt.isep.nsheets.shared.services.WorkbooksServiceAsync;
 
 public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresenter.MyProxy> {
 
 	private MyView view;
 
 	interface MyView extends View {
-		void setContents();
-
-		void addClickHandler(ClickHandler ch);
+            void addLoginHandler(ClickHandler ch);
+            
+            String getEmail();
+            String getPassword();
 	}
 
 	@NameToken(NameTokens.login)
@@ -31,9 +38,14 @@ public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresen
 
 	@Inject
 	LoginPresenter(EventBus eventBus, MyView view, MyProxy proxy) {
-		super(eventBus, view, proxy, ApplicationPresenter.SLOT_CONTENT);
+            super(eventBus, view, proxy, ApplicationPresenter.SLOT_CONTENT);
 
-		this.view = view;
+            this.view = view;
+            
+            this.view.addLoginHandler(event -> {
+                String email = this.view.getEmail();
+                String password = this.view.getPassword();
+            });
 	}
 
 	private void refreshView() {
@@ -41,10 +53,10 @@ public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresen
 	
 	@Override
 	protected void onReveal() {
-		super.onReveal();
+            super.onReveal();
 
-		SetPageTitleEvent.fire("Login", "Login on the page", "", "", this);
+            SetPageTitleEvent.fire("Login", "Login on NSheets booooooyyyyy", "", "", this);
 
-		refreshView();
+            refreshView();
 	}
 }
--- nsheets/src/main/java/pt/isep/nsheets/client/application/login/LoginView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/login/LoginView.java
@@ -1,41 +1,46 @@
 package pt.isep.nsheets.client.application.login;
 
 
-import javax.inject.Inject;
-
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 
 import gwt.material.design.client.ui.MaterialButton;
+import gwt.material.design.client.ui.MaterialTextBox;
+import javax.inject.Inject;
 
 class LoginView extends ViewImpl implements LoginPresenter.MyView {
+    interface Binder extends UiBinder<Widget, LoginView> {
+    }
 
-	interface Binder extends UiBinder<Widget, LoginView> {
-	}
-	
-	@UiField
-	HTMLPanel htmlPanel;
+    @UiField
+    MaterialTextBox txtEmail;
 
-	@UiField
-	MaterialButton newWorkbookButton;
-	
-	@Inject
-	LoginView(Binder uiBinder) {
-		initWidget(uiBinder.createAndBindUi(this));		
-	}
+    @UiField
+    MaterialTextBox txtPassword;
 
-	@Override
-	public void setContents() {
-	}
-	
-	@Override
-	public void addClickHandler(ClickHandler ch) {
-		// TODO Auto-generated method stub
-		
-		newWorkbookButton.addClickHandler( ch );
-	}	
+    @UiField
+    MaterialButton btnLogin;
+
+    @Inject
+    LoginView(Binder uiBinder) {
+        initWidget(uiBinder.createAndBindUi(this));		
+    }
+    
+    @Override
+    public void addLoginHandler(ClickHandler ch) {
+        btnLogin.addClickHandler(ch);
+    }
+    
+    @Override
+    public String getEmail() {
+        return txtEmail.getText();
+    }
+    
+    @Override
+    public String getPassword() {
+        return txtPassword.getText();
+    }
 }
