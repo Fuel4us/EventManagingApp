3169b99406af9889aea6cc670fc3566ddb44a5c7
Gonçalo Silva <1161140@isep.ipp.pt>
2018-05-30 16:41:08 +0100

    #16 Added a (very) basic calendar UI

--- nsheets/src/main/java/pt/isep/nsheets/client/application/calendar/CalendarView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/calendar/CalendarView.java
@@ -1,8 +1,10 @@
 package pt.isep.nsheets.client.application.calendar;
 
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import gwt.material.design.client.ui.MaterialDatePicker;
 
 import javax.inject.Inject;
 
@@ -10,9 +12,11 @@ class CalendarView extends ViewImpl implements CalendarPresenter.MyView {
     interface Binder extends UiBinder<Widget, CalendarView> {
     }
 
+    @UiField
+    MaterialDatePicker dp;
+
     @Inject
     CalendarView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
     }
-
 }
