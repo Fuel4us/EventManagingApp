0a6a573cec5b269aa959c6e8e99cf62c5ddd6ccc
Gonçalo Silva <1161140@isep.ipp.pt>
2018-06-01 23:45:30 +0100

    Rewrote event creation modal and added cards for created events

--- nsheets/src/main/java/pt/isep/nsheets/client/application/calendar/CalendarPresenter.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/calendar/CalendarPresenter.java
@@ -1,5 +1,7 @@
 package pt.isep.nsheets.client.application.calendar;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
@@ -7,14 +9,23 @@ import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import gwt.material.design.client.ui.MaterialToast;
 import pt.isep.nsheets.client.application.ApplicationPresenter;
 import pt.isep.nsheets.client.event.SetPageTitleEvent;
 import pt.isep.nsheets.client.place.NameTokens;
+import pt.isep.nsheets.server.lapr4.red.s1.core.n1161140.calendar.domain.CalendarEvent;
+import pt.isep.nsheets.shared.services.*;
+
+import java.util.ArrayList;
 
 public class CalendarPresenter extends Presenter<CalendarPresenter.MyView, CalendarPresenter.MyProxy> {
 
+    private MyView view;
+
     interface MyView extends View {
+        void setContents(ArrayList<CalendarEventDTO> contents);
     }
+
 
     @NameToken(NameTokens.calendar)
     @ProxyStandard
@@ -31,6 +42,25 @@ public class CalendarPresenter extends Presenter<CalendarPresenter.MyView, Calen
         super.onReveal();
 
         SetPageTitleEvent.fire("Calendar", "Manage your calendars and events", "", "", this);
+
+        refreshView();
+    }
+
+    private void refreshView() {
+        CalendarEventServiceAsync workbooksSvc = GWT.create(CalendarEventService.class);
+
+        // Set up the callback object.
+        AsyncCallback<ArrayList<CalendarEventDTO>> callback = new AsyncCallback<ArrayList<CalendarEventDTO>>() {
+            public void onFailure(Throwable caught) {
+                // TODO: Do something with errors.
+            }
+
+            public void onSuccess(ArrayList<CalendarEventDTO> result) {
+                view.setContents(result);
+            }
+        };
+
+        workbooksSvc.getWorkbooks(callback);
     }
 
 }
--- nsheets/src/main/java/pt/isep/nsheets/client/application/calendar/CalendarView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/calendar/CalendarView.java
@@ -1,60 +1,181 @@
 package pt.isep.nsheets.client.application.calendar;
 
-import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
-import gwt.material.design.client.ui.MaterialButton;
-import gwt.material.design.client.ui.MaterialModal;
-import gwt.material.design.client.ui.MaterialToast;
+import gwt.material.design.addins.client.emptystate.MaterialEmptyState;
+import gwt.material.design.addins.client.timepicker.MaterialTimePicker;
+import gwt.material.design.client.constants.ButtonType;
+import gwt.material.design.client.constants.DatePickerContainer;
+import gwt.material.design.client.constants.ModalType;
+import gwt.material.design.client.ui.*;
+import pt.isep.nsheets.shared.services.CalendarEventDTO;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Date;
 
 class CalendarView extends ViewImpl implements CalendarPresenter.MyView {
+
     interface Binder extends UiBinder<Widget, CalendarView> {
+
     }
 
-    @Inject
-    CalendarView(Binder uiBinder) {
-        initWidget(uiBinder.createAndBindUi(this));
-    }
+    @UiField
+    HTMLPanel htmlPanel;
+
+    @UiField
+    MaterialEmptyState emptyState;
 
     @UiField
     MaterialButton createCalendarEventButton;
 
-    @UiField
-    MaterialButton confirmCalendarEventButton;
+    MaterialModal modal;
 
-    @UiField
-    MaterialButton cancelCalendarEventButton;
+    MaterialTextBox calendarEventNameTextBox;
 
-    @UiField
-    MaterialModal newEventModal;
+    MaterialTextArea calendarEventDescriptionTextArea;
 
-    @UiHandler("createCalendarEventButton")
-    void onCalendarEventCreation(ClickEvent e) {
-        newEventModal.open();
+    MaterialDatePicker calendarEventDatePicker;
+
+    MaterialTimePicker calendarEventTimePicker;
+
+    MaterialRange calendarEventDuration;
+
+    @Inject
+    CalendarView(Binder uiBinder) {
+        initWidget(uiBinder.createAndBindUi(this));
+
+        createCalendarEventButton.addClickHandler(event -> openModal());
     }
 
-    @UiHandler("confirmCalendarEventButton")
-    void onCalendarEventConfirmation(ClickEvent e) {
-        newEventModal.close();
+    void openModal() {
+        createModal();
+
+        modal.open();
+    }
+
+    void createEvent() {
+        CalendarEventDTO calendarEvent = new CalendarEventDTO(calendarEventNameTextBox.getValue(), calendarEventDescriptionTextArea.getValue(), calendarEventDatePicker.getValue(), calendarEventTimePicker.getValue(), calendarEventDuration.getValue());
+
+        modal.close();
 
         MaterialToast.fireToast("Event created successfully", "rounded");
 
-        /*CalendarEvent calendarEvent = new CalendarEvent();
-        if (calendarEvent != null) {
-            MaterialToast.fireToast("Event created successfully", "rounded");
-        } else {
-            MaterialToast.fireToast("An error occurred... Please try again", "rounded");
-        }*/
+        emptyState.setVisible(false);
+        createCard(calendarEvent);
     }
 
+    void closeModal() {
+        modal.close();
+    }
 
-    @UiHandler("cancelCalendarEventButton")
-    void onCalendarEventCancelation(ClickEvent e) {
-        newEventModal.close();
+    private void createModal() {
+        modal = new MaterialModal();
+        modal.setType(ModalType.FIXED_FOOTER);
+        modal.setDismissible(false);
+        htmlPanel.add(modal);
+
+        MaterialModalContent content = new MaterialModalContent();
+        modal.add(content);
+
+        MaterialTitle title = new MaterialTitle();
+        title.setTitle("New Event");
+        content.add(title);
+
+        calendarEventNameTextBox = new MaterialTextBox();
+        calendarEventNameTextBox.setLabel("Name");
+        content.add(calendarEventNameTextBox);
+
+        calendarEventDescriptionTextArea = new MaterialTextArea();
+        calendarEventDescriptionTextArea.setLabel("Description");
+        content.add(calendarEventDescriptionTextArea);
+
+        MaterialRow row = new MaterialRow();
+        content.add(row);
+
+        MaterialColumn aColumn = new MaterialColumn();
+        aColumn.setGrid("s6");
+        row.add(aColumn);
+
+        MaterialColumn anotherColumn = new MaterialColumn();
+        anotherColumn.setGrid("s4");
+        row.add(anotherColumn);
+
+        calendarEventDatePicker = new MaterialDatePicker();
+        calendarEventDatePicker.setPlaceholder("Date");
+        calendarEventDatePicker.setDateMin(new Date());
+        calendarEventDatePicker.setContainer(DatePickerContainer.BODY);
+        aColumn.add(calendarEventDatePicker);
+
+        calendarEventTimePicker = new MaterialTimePicker();
+        calendarEventTimePicker.setPlaceholder("Time");
+        calendarEventTimePicker.setHour24(true);
+        anotherColumn.add(calendarEventTimePicker);
+
+        MaterialLabel durationLabel = new MaterialLabel();
+        durationLabel.setText("Duration in minutes");
+        content.add(durationLabel);
+
+        calendarEventDuration = new MaterialRange();
+        calendarEventDuration.setMin(1);
+        calendarEventDuration.setMax(60);
+        calendarEventDuration.setValue(15);
+        content.add(calendarEventDuration);
+
+        MaterialModalFooter modalFooter = new MaterialModalFooter();
+        modal.add(modalFooter);
+
+        MaterialButton cancelCalendarEventButton = new MaterialButton();
+        cancelCalendarEventButton.setText("Close");
+        cancelCalendarEventButton.setType(ButtonType.FLAT);
+        cancelCalendarEventButton.addClickHandler(event -> closeModal());
+        modalFooter.add(cancelCalendarEventButton);
+
+        MaterialButton confirmCalendarEventButton = new MaterialButton();
+        confirmCalendarEventButton.setText("Create");
+        confirmCalendarEventButton.setType(ButtonType.FLAT);
+        confirmCalendarEventButton.addClickHandler(event -> createEvent());
+        modalFooter.add(confirmCalendarEventButton);
+    }
+
+    private void createCard(CalendarEventDTO calendarEvent) {
+        MaterialCard card = new MaterialCard();
+        htmlPanel.add(card);
+
+        MaterialCardContent content = new MaterialCardContent();
+        card.add(content);
+
+        MaterialCardTitle title = new MaterialCardTitle();
+        title.setText(calendarEvent.getName());
+        content.add(title);
+
+        MaterialLabel descriptionLabel = new MaterialLabel();
+        descriptionLabel.setText(calendarEvent.getDescription());
+        content.add(descriptionLabel);
+
+        DateTimeFormat dateFormat = DateTimeFormat.getFormat("dd/MM/yyyy");
+
+        MaterialLabel dateLabel = new MaterialLabel();
+        dateLabel.setText(dateFormat.format(calendarEvent.getDate()));
+        content.add(dateLabel);
+
+        DateTimeFormat timeFormat = DateTimeFormat.getFormat("HH:mm");
+
+        MaterialLabel timeLabel = new MaterialLabel();
+        timeLabel.setText(timeFormat.format(calendarEvent.getTime()));
+        content.add(timeLabel);
+
+        MaterialLabel durationLabel = new MaterialLabel();
+        durationLabel.setText(calendarEvent.getDuration().toString() + " minutes");
+        content.add(durationLabel);
+    }
+
+    @Override
+    public void setContents(ArrayList<CalendarEventDTO> contents) {
+
     }
 }
