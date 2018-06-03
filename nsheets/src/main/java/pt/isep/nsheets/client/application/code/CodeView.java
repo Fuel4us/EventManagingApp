ddc18a4ecbc72ae6e3753528dc21bc95b749b622
Josué <1160713@isep.ipp.pt>
2018-06-03 21:29:37 +0100

    lang07.1 - Corrections
    Signed-off-by: Josué <1160713@isep.ipp.pt>

--- nsheets/src/main/java/pt/isep/nsheets/client/application/code/CodeView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/code/CodeView.java
@@ -1,19 +1,23 @@
 package pt.isep.nsheets.client.application.code;
 
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
-import gwt.material.design.addins.client.window.MaterialWindow;
 import gwt.material.design.client.ui.MaterialButton;
 import gwt.material.design.client.ui.MaterialTextArea;
 import gwt.material.design.client.ui.MaterialTextBox;
-import org.apache.jasper.tagplugins.jstl.core.Out;
-import pt.isep.nsheets.client.application.form.FormPresenter;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import pt.isep.nsheets.shared.core.vb.EvalVisitor;
+import pt.isep.nsheets.shared.core.vb.Value;
+import pt.isep.nsheets.shared.core.vb.VbLexer;
+import pt.isep.nsheets.shared.core.vb.VbParser;
 
 class CodeView extends ViewImpl implements CodePresenter.MyView {
 
@@ -22,7 +26,7 @@ class CodeView extends ViewImpl implements CodePresenter.MyView {
     @UiField
     MaterialButton runButton;
     @UiField
-    MaterialTextBox OutputTextBox;
+    MaterialTextArea OutputText;
 
     interface Binder extends UiBinder<Widget, CodeView> {
     }
@@ -35,8 +39,27 @@ class CodeView extends ViewImpl implements CodePresenter.MyView {
 
     private void initialize() {
         runButton.addClickHandler(event -> {
+
+            //teste
+            Map<String, Value> cells = new HashMap<>();
+
+            cells.put("A1", new Value(Double.valueOf(0)));
+
+            VbLexer lexer = new VbLexer(new ANTLRInputStream(CodeArea.getText()));
+            VbParser parser = new VbParser(new CommonTokenStream(lexer));
+            ParseTree tree = parser.parse();
+            EvalVisitor visitor = new EvalVisitor(cells);
+            visitor.visit(tree);
+
             //String codeAread = CodeArea.getText();
-            OutputTextBox.setText(CodeArea.getText()); //test
+            OutputText.setText(visitor.getOutput());
+
+            //teste
+            System.out.println("Valor $A1: " + cells.entrySet().iterator().next().getValue().asDouble());
+
+            System.out.println("Output:\n");
+
+            System.out.println(visitor.getOutput());
         });
     }
 }
