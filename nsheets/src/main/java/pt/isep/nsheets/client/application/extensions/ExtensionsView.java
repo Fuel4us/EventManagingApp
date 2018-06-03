c0f9bdbcd74bc11010e21a134ecfe88f8a85af04
1160629 <1160629@isep.ipp.pt>
2018-06-03 11:31:18 +0100

    Added example extension configuration to UI

--- nsheets/src/main/java/pt/isep/nsheets/client/application/extensions/ExtensionsView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/extensions/ExtensionsView.java
@@ -2,63 +2,35 @@ package pt.isep.nsheets.client.application.extensions;
 
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
-import gwt.material.design.client.constants.Flex;
-import gwt.material.design.client.constants.WavesType;
-import gwt.material.design.client.ui.MaterialLink;
-import gwt.material.design.client.ui.MaterialRow;
-import gwt.material.design.client.ui.MaterialTab;
-import gwt.material.design.client.ui.MaterialTabItem;
-import gwt.material.design.client.ui.html.Div;
-import gwt.material.design.client.ui.html.Label;
-import pt.isep.nsheets.shared.ext.Extension;
-import pt.isep.nsheets.shared.ext.ExtensionManager;
+import gwt.material.design.client.ui.MaterialButton;
+import gwt.material.design.client.ui.MaterialInput;
+import gwt.material.design.client.ui.MaterialToast;
 
 import javax.inject.Inject;
 
 class ExtensionsView extends ViewImpl implements ExtensionsPresenter.MyView {
 
-    interface Binder extends UiBinder<Widget, ExtensionsView> {
-    }
+    @UiField
+    MaterialButton confirmButton;
+    @UiField
+    MaterialInput BgColorPos;
+    @UiField
+    MaterialInput FgColorPos;
+    @UiField
+    MaterialInput BgColorNeg;
+    @UiField
+    MaterialInput FgColorNeg;
 
     @Inject
     ExtensionsView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
-        loadExtensions();
+        confirmButton.addClickHandler(event -> {
+            MaterialToast.fireToast("Color picked for Positive Background Color " + BgColorPos.getValue());
+        });
     }
 
-    @UiField
-    MaterialTab extensionTab;
-
-    @UiField
-    MaterialRow extensionRow;
-
-
-
-    private void loadExtensions(){
-       // ExtensionManager extensionManager = ExtensionManager.getInstance();
-       // Extension[] extensions=extensionManager.getExtensions();
-        for(int i=0;i<5;i++){
-            MaterialTabItem materialTabItem = new MaterialTabItem();
-            materialTabItem.setWaves(WavesType.LIGHT);
-            MaterialLink materialLink = new MaterialLink();
-            materialLink.setText("Extension "+i);
-
-            materialLink.setHref("#extension"+i);
-            materialTabItem.add(materialLink);
-            extensionTab.add(materialTabItem);
-            Div test = new Div();
-            test.setId("#extension"+i);
-            Label label = new Label();
-            label.setText("Test for "+i);
-            test.add(label);
-            test.setVisible(false);
-            extensionRow.add(test);
-        }
+    interface Binder extends UiBinder<Widget, ExtensionsView> {
     }
-
-
-
 }
