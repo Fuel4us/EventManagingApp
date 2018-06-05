86543d1bb2864705e2f9be0ec96beef97f633cb4
Gonçalo Fonseca (1150503) <1150503@isep.ipp.pt>
2018-06-05 01:13:11 +0100

    [Implementation] Created controller to have pdf exporter method

---
+++ server/src/main/java/pt/isep/nsheets/server/lapr4/green/s1/ipc/n1150503/ExportPDF/ExportPdfController.java
@@ -0,0 +1,18 @@
+package pt.isep.nsheets.server.lapr4.green.s1.ipc.n1150503.ExportPDF;
+
+import pt.isep.nsheets.shared.core.Workbook;
+
+import java.io.IOException;
+
+/**
+ * @author Gonçalo Fonseca <1150503@isep.ipp.pt>
+ */
+public class ExportPdfController {
+
+    public ExportPdfController() {
+    }
+
+    public void exportWorkbook(Workbook workbook) {
+        String pdf = "../pdf.pdf";
+    }
+}
