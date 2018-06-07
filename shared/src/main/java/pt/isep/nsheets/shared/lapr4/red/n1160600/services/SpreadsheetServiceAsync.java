bb633e1287785461124871e078122d42a3a73eb5
Joao Reis (1160600) <1160600@isep.ipp.pt>
2018-06-07 16:43:05 +0100

    [implementation] sortCells reverting commit

--- shared/src/main/java/pt/isep/nsheets/shared/lapr4/red/n1160600/services/SpreadsheetServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package pt.isep.nsheets.shared.lapr4.red.n1160600.services;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import pt.isep.nsheets.shared.core.Spreadsheet;
-import pt.isep.nsheets.shared.lapr4.red.s1.core.n1161292.services.SpreadsheetDTO;
-
-public interface SpreadsheetServiceAsync {
-    void sortCells(String stringCell1, String stringCell2, SpreadsheetDTO spreadSheet, boolean ascendant, AsyncCallback<SpreadsheetDTO> callback);
-}
