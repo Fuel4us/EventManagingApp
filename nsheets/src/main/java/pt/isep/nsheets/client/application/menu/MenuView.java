be469eb981073937e7d9dc35d4e5910aa460a66e
João Pereira (1150478) <1150478@isep.ipp.pt>
2018-06-07 16:23:06 +0100

    [Implementation] Core02.2 - add menu option functional. [Documentation] Core02.2 - changes on readMe.md, documentation added.

--- nsheets/src/main/java/pt/isep/nsheets/client/application/menu/MenuView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/menu/MenuView.java
@@ -6,12 +6,12 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import gwt.material.design.client.ui.MaterialButton;
 
 import gwt.material.design.client.ui.MaterialNavBar;
 import gwt.material.design.client.ui.MaterialSideNavPush;
-import gwt.material.design.client.ui.html.ListItem;
 
-class MenuView extends ViewWithUiHandlers<MenuUiHandlers> implements MenuPresenter.MyView {
+public class MenuView extends ViewWithUiHandlers<MenuUiHandlers> implements MenuPresenter.MyView {
 
     interface Binder extends UiBinder<Widget, MenuView> {
 
@@ -20,17 +20,17 @@ class MenuView extends ViewWithUiHandlers<MenuUiHandlers> implements MenuPresent
     @UiField
     MaterialNavBar navBar;
     @UiField
-    MaterialSideNavPush sideNav;
+    static MaterialSideNavPush sideNav;
+    @UiField
+    MaterialButton testAddMenu;
 
     @Inject
     MenuView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
     }
 
-    void newOptionCreator(String name) {
-        ListItem newItem = new ListItem();
-        newItem.setTitle(name);
-        sideNav.add(newItem);
+    public static MaterialSideNavPush getSideNav() {
+        return sideNav;
     }
 
 }
