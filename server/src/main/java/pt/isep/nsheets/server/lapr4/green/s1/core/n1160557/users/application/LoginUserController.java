282185efe8e19ae2a70534c074c6edbacab24a1b
Tiago João Santos Rios <1161292@isep.ipp.pt>
2018-06-09 00:44:31 +0100

    Fixed Login issues

--- server/src/main/java/pt/isep/nsheets/server/lapr4/green/s1/core/n1160557/users/application/LoginUserController.java
+++ server/src/main/java/pt/isep/nsheets/server/lapr4/green/s1/core/n1160557/users/application/LoginUserController.java
@@ -13,12 +13,12 @@ import pt.isep.nsheets.shared.services.UserDTO;
 public class LoginUserController {
 
     private List<User> users = Arrays.asList(
-            new User("1160557@isep.ipp.pt", "Hilário", "coelho98", "123asd", true),
-            new User("1160634@isep.ipp.pt", "Pedro", "vieira98", "123asd", false),
-            new User("1161140@isep.ipp.pt", "Gonçalo", "silva97", "123asd", false),
-            new User("1160630@isep.ipp.pt", "José Pedro", "monteiro98", "123asd", false),
-            new User("1160600@isep.ipp.pt", "João", "reis98", "123asd", false),
-            new User("1160629@isep.ipp.pt", "José Pedro", "espirito_santo97", "123asd", false)
+            new User("1160557@isep.ipp.pt", "Hilário", "coelho98", "123Asd", true),
+            new User("1160634@isep.ipp.pt", "Pedro", "vieira98", "123Asd", false),
+            new User("1161140@isep.ipp.pt", "Gonçalo", "silva97", "123Asd", false),
+            new User("1160630@isep.ipp.pt", "José Pedro", "monteiro98", "123Asd", false),
+            new User("1160600@isep.ipp.pt", "João", "reis98", "123Asd", false),
+            new User("1160629@isep.ipp.pt", "José Pedro", "espirito_santo97", "123Asd", false)
     );
 
     public LoginUserController() {
