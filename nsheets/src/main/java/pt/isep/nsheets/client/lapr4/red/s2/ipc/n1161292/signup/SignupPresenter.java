c26852b13854e7fcf222225badd56570f3e1c8b0
Tiago João Santos Rios <1161292@isep.ipp.pt>
2018-06-09 22:46:42 +0100

    Added redirection to Login Page after signup

--- nsheets/src/main/java/pt/isep/nsheets/client/lapr4/red/s2/ipc/n1161292/signup/SignupPresenter.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/lapr4/red/s2/ipc/n1161292/signup/SignupPresenter.java
@@ -12,12 +12,17 @@ import com.gwtplatform.mvp.client.annotations.NoGatekeeper;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 import gwt.material.design.client.ui.MaterialTextBox;
 import gwt.material.design.client.ui.MaterialToast;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import pt.isep.nsheets.client.application.ApplicationPresenter;
 import pt.isep.nsheets.client.event.SetPageTitleEvent;
 import pt.isep.nsheets.client.place.NameTokens;
+import pt.isep.nsheets.client.place.ParameterTokens;
 import pt.isep.nsheets.shared.lapr4.red.s2.ipc.n1161292.services.SignupService;
 import pt.isep.nsheets.shared.lapr4.red.s2.ipc.n1161292.services.SignupServiceAsync;
 import pt.isep.nsheets.shared.services.UserDTO;
@@ -29,23 +34,22 @@ import pt.isep.nsheets.shared.services.UserDTO;
 public class SignupPresenter extends Presenter<SignupPresenter.MyView, SignupPresenter.MyProxy> {
 
     private MyView view;
+    private final PlaceManager placeManager;
 
     interface MyView extends View {
-
         void submitButtonClickHandler(ClickHandler ch);
 
         String username();
-
         String password();
-
         String email();
-
         String name();
         
         MaterialTextBox txtUsername();
         MaterialTextBox txtPassword();
         MaterialTextBox txtEmail();
         MaterialTextBox txtName();
+        
+        void clearFields();
     }
 
     @NameToken(NameTokens.SIGNUP)
@@ -58,6 +62,7 @@ public class SignupPresenter extends Presenter<SignupPresenter.MyView, SignupPre
     SignupPresenter(EventBus eventBus, SignupPresenter.MyView view, SignupPresenter.MyProxy proxy, PlaceManager placeManager) {
         super(eventBus, view, proxy, ApplicationPresenter.SLOT_CONTENT);
         this.view = view;
+        this.placeManager = placeManager;
 
         createSubmitButtonHandler();
     }
@@ -69,22 +74,24 @@ public class SignupPresenter extends Presenter<SignupPresenter.MyView, SignupPre
             AsyncCallback<UserDTO> callback = new AsyncCallback<UserDTO>() {
                 @Override
                 public void onFailure(Throwable caught) {
-                    MaterialToast.fireToast("Error configuring Conditionalextension! " + caught.getMessage());
+                    MaterialToast.fireToast("Error when registering new user.! " + caught.getMessage());
                 }
 
                 @Override
                 public void onSuccess(UserDTO result) {
-                    MaterialToast.fireToast("Conditionalextension conditional configured!" + result.getName());
+                    MaterialToast.fireToast("Signup made with success! Welcome, " + result.getName());
+                    SignupPresenter.this.view.clearFields();
+                    redirectToLoginPage();
                 }
             };
             
-            boolean validation = false;
+            boolean validation;
             
             validation = this.view.txtEmail().validate();
             validation = this.view.txtUsername().validate();
             validation = this.view.txtName().validate();
             validation = this.view.txtPassword().validate();
-            MaterialToast.fireToast(String.valueOf(validation));
+            
             if(validation){
                 UserDTO dto = new UserDTO(this.view.email(), this.view.name(), this.view.username(), this.view.password(), false, new ArrayList<>());
 
@@ -92,7 +99,16 @@ public class SignupPresenter extends Presenter<SignupPresenter.MyView, SignupPre
             }
         });
     }
+    
+    private void redirectToLoginPage(){
+        String token = placeManager
+                .getCurrentPlaceRequest()
+                .getParameter(ParameterTokens.REDIRECT, NameTokens.login);
+        PlaceRequest placeRequest = new PlaceRequest.Builder().nameToken(token).build();
 
+        placeManager.revealPlace(placeRequest);
+    }
+    
     @Override
     protected void onReveal() {
         super.onReveal();
