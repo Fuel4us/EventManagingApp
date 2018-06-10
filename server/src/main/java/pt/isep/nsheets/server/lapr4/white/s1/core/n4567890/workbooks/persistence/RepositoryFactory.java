335329fd65518e7c754db22208a5b7403e25aa93
Hernani Gil(1050475) <1050475@isep.ipp.pt>
2018-06-10 16:56:58 +0100

    [#55] Persistence and corrections

--- server/src/main/java/pt/isep/nsheets/server/lapr4/white/s1/core/n4567890/workbooks/persistence/RepositoryFactory.java
+++ server/src/main/java/pt/isep/nsheets/server/lapr4/white/s1/core/n4567890/workbooks/persistence/RepositoryFactory.java
@@ -5,7 +5,7 @@
  */
 package pt.isep.nsheets.server.lapr4.white.s1.core.n4567890.workbooks.persistence;
 
-import pt.isep.nsheets.server.lapr4.blue.s1.n1050475.persistence.ConditionalRepository;
+import pt.isep.nsheets.server.lapr4.blue.n1050475.s1.persistence.ConditionalRepository;
 import pt.isep.nsheets.server.lapr4.green.s1.core.n1160557.users.persistence.UserRepository;
 import pt.isep.nsheets.server.lapr4.green.s1.ipc.n1160815.users.persistence.MessagesRepository;
 //import pt.isep.nsheets.server.lapr4.green.s1.core.n1160832.spreadsheets.persistence.jpa.SpreadsheetRepository;
