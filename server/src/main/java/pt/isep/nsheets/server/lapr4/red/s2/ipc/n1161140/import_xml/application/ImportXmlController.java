9880bde44b9f9c7466d4abc73b813567fbb9d78d
Gonçalo Silva <1161140@isep.ipp.pt>
2018-06-12 00:12:18 +0100

    #35 Updated the XML parser

--- server/src/main/java/pt/isep/nsheets/server/lapr4/red/s2/ipc/n1161140/import_xml/application/ImportXmlController.java
+++ server/src/main/java/pt/isep/nsheets/server/lapr4/red/s2/ipc/n1161140/import_xml/application/ImportXmlController.java
@@ -1,22 +1,20 @@
 package pt.isep.nsheets.server.lapr4.red.s2.ipc.n1161140.import_xml.application;
 
+import com.google.gwt.core.client.GWT;
+
+import java.io.File;
+
 public class ImportXmlController {
 
     public void importWorkbooks() {
-        ImportXmlService service = new ImportXmlService();
+        ImportXmlService service = new ImportXmlService(new File(GWT.getModuleBaseURL()));
 
         service.importWorkbooks();
     }
 
     public void importSpreadsheets() {
-        ImportXmlService service = new ImportXmlService();
-
-        service.importSpreadsheets();
     }
 
     public void importCells() {
-        ImportXmlService service = new ImportXmlService();
-
-        service.importCells();
     }
 }
