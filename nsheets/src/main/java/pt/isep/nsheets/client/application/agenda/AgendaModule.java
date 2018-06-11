a39f7a5d070c093c6c5377f3404451fd95adf079
Pedro Alves <1150372@isep.ipp.pt>
2018-06-11 21:53:45 +0100

    Agenda.

--- nsheets/src/main/java/pt/isep/nsheets/client/application/agenda/AgendaModule.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/agenda/AgendaModule.java
@@ -1,16 +1,16 @@
-//package pt.isep.nsheets.client.application.agenda;
-//
-//import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
-//
-///**
-// *
-// * @author Pedro Alves 1150372@isep.ipp.pt
-// */
-//public class AgendaModule extends AbstractPresenterModule {
-//
-//    @Override
-//    protected void configure() {
-//        bindPresenter(AgendaPresenter.class, AgendaPresenter.MyView.class, AgendaView.class, AgendaPresenter.MyProxy.class);
-//    }
-//
-//}
+package pt.isep.nsheets.client.application.agenda;
+
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+
+/**
+ *
+ * @author Pedro Alves 1150372@isep.ipp.pt
+ */
+public class AgendaModule extends AbstractPresenterModule {
+
+    @Override
+    protected void configure() {
+        bindPresenter(AgendaPresenter.class, AgendaPresenter.MyView.class, AgendaView.class, AgendaPresenter.MyProxy.class);
+    }
+
+}
