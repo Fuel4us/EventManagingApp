6c60b334a3567a9ee548caa90f5465b19db8dea2
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-06-12 04:59:42 +0100

    #62 [Implementation] UI and UI handlers

--- nsheets/src/main/java/pt/isep/nsheets/client/application/form/FormView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/form/FormView.java
@@ -1,10 +1,12 @@
 package pt.isep.nsheets.client.application.form;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
@@ -12,6 +14,10 @@ import gwt.material.design.addins.client.combobox.MaterialComboBox;
 import gwt.material.design.addins.client.window.MaterialWindow;
 import gwt.material.design.client.ui.MaterialButton;
 import gwt.material.design.client.ui.MaterialTextBox;
+import gwt.material.design.client.ui.MaterialToast;
+import pt.isep.nsheets.shared.services.FormsVariablesService;
+import pt.isep.nsheets.shared.services.FormsVariablesServiceAsync;
+
 import javax.inject.Inject;
 
 /**
@@ -33,10 +39,10 @@ public class FormView extends ViewImpl implements FormPresenter.MyView {
     static MaterialWindow window, windowTab;
 
     @UiField
-    MaterialButton btnAddRow, btnRemoveRow, btnEditRow, btnPlayForm1, btnPlayForm2, btnPlayForm3;
+    MaterialButton btnAddRow, btnRemoveRow, btnEditRow, btnPlayForm1, btnPlayForm2, btnPlayForm3,checkBtn;
 
     @UiField
-    MaterialTextBox txtAreaRemove, txtAreaEdit, txtAreaAdd;
+    MaterialTextBox txtAreaRemove, txtAreaEdit, txtAreaAdd,nameTextBox;
 
     @UiField
     MaterialComboBox comboWidgets;
@@ -55,6 +61,124 @@ public class FormView extends ViewImpl implements FormPresenter.MyView {
         table.setBorderWidth(3);
         table.setWidget(0, 0, null);
         initWidget(uiBinder.createAndBindUi(this));
+        checkBtn.addClickHandler(clickEvent -> {
+            if(nameTextBox.getText().isEmpty()){
+                MaterialToast.fireToast("Please Insert a Name");
+            }else{
+                FormsVariablesServiceAsync async = GWT.create(FormsVariablesService.class);
+                AsyncCallback<String> callback = new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        MaterialToast.fireToast("Error! " + throwable.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(String s) {
+                        MaterialToast.fireToast("Message: "+s);
+                            txtAreaAdd.setText(s);
+                    }
+                };
+
+                async.getContentFromVariableByName(nameTextBox.getText(),comboWidgets.getSelectedIndex(),callback);
+            }
+        });
+
+        btnAddRow.addClickHandler(clickEvent -> {
+
+            MaterialButton resolveButton = new MaterialButton("Button");
+            MaterialTextBox writeLabel1 = new MaterialTextBox();
+            MaterialTextBox writeLabel2 = new MaterialTextBox();
+            i++;
+            switch (comboWidgets.getSelectedIndex()) {
+                case 0:
+                    writeLabel1.setEnabled(false);
+                    table.setWidget(i, 1, writeLabel1);
+
+                    break;
+                case 1:
+                    if(!nameTextBox.getText().isEmpty()){
+                        FormsVariablesServiceAsync async = GWT.create(FormsVariablesService.class);
+                        AsyncCallback<Void> callback = new AsyncCallback<Void>() {
+                            @Override
+                            public void onFailure(Throwable throwable) {
+                                MaterialToast.fireToast("Error"+throwable.getMessage());
+                            }
+
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                MaterialToast.fireToast("Temporary Variable Saved Successfully");
+                            }
+                        };
+                        async.addTemporaryVariable(nameTextBox.getText(),txtAreaAdd.getText(),comboWidgets.getSelectedIndex(),callback);
+                        MaterialButton button = new MaterialButton(txtAreaAdd.getText());
+                        table.setWidget(i,1,button);
+                    }else {
+                        table.setWidget(i, 1, resolveButton);
+                    }
+                    break;
+                case 2:
+                    writeLabel1.setEnabled(false);
+                    writeLabel1.setGrid("s12");
+                    if(!nameTextBox.getText().isEmpty()){
+                        FormsVariablesServiceAsync async = GWT.create(FormsVariablesService.class);
+                        AsyncCallback<Void> callback = new AsyncCallback<Void>() {
+                            @Override
+                            public void onFailure(Throwable throwable) {
+                                MaterialToast.fireToast("Error" + throwable.getMessage());
+                            }
+
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                MaterialToast.fireToast("Temporary Variable Saved Successfully");
+                            }
+                        };
+                        async.addTemporaryVariable(nameTextBox.getText(),txtAreaAdd.getText(),comboWidgets.getSelectedIndex(),callback);
+                    }
+                     if (txtAreaAdd.getText().isEmpty()) {
+                        writeLabel1.setPlaceholder("Write here:");
+                    } else {
+                        writeLabel1.setPlaceholder(txtAreaAdd.getText());
+                    }
+                    table.setWidget(i, 1, writeLabel1);
+                    break;
+                case 3:
+                    writeLabel1.setEnabled(false);
+                    writeLabel1.setGrid("s12");
+                    if(!nameTextBox.getText().isEmpty()){
+                        FormsVariablesServiceAsync async = GWT.create(FormsVariablesService.class);
+                        AsyncCallback<Void> callback = new AsyncCallback<Void>() {
+                            @Override
+                            public void onFailure(Throwable throwable) {
+                                MaterialToast.fireToast("Error" + throwable.getMessage());
+                            }
+
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                MaterialToast.fireToast("Temporary Variable Saved Successfully");
+                            }
+                        };
+                        async.addTemporaryVariable(nameTextBox.getText(),txtAreaAdd.getText(),comboWidgets.getSelectedIndex(),callback);
+
+                    }
+                    if (txtAreaAdd.getText().isEmpty()) {
+                        writeLabel1.setPlaceholder("Write here:");
+                    } else {
+                        writeLabel1.setPlaceholder(txtAreaAdd.getText());
+                    }
+                    writeLabel2.setEnabled(false);
+                    writeLabel2.setGrid("s12");
+                    writeLabel2.setPlaceholder("Write here:");
+                    table.setWidget(i, 1, writeLabel1);
+                    table.setWidget(i, 2, writeLabel2);
+                    break;
+                default:
+                    break;
+            }
+
+            window.add(table);
+
+
+        });
     }
 
     public static MaterialWindow getWindow() {
@@ -81,56 +205,9 @@ public class FormView extends ViewImpl implements FormPresenter.MyView {
         windowTab.open();
     }
 
-    /**
-     * Add Row button action.
-     *
-     * @param e
-     */
-    @UiHandler("btnAddRow")
-    void addRowButton(ClickEvent e) {
-        MaterialButton resolveButton = new MaterialButton("Resolve");
-        MaterialTextBox writeLabel1 = new MaterialTextBox();
-        MaterialTextBox writeLabel2 = new MaterialTextBox();
-        i++;
-        switch (comboWidgets.getSelectedIndex()) {
-            case 0:
-                writeLabel1.setEnabled(false);
-                table.setWidget(i, 1, writeLabel1);
-                break;
-            case 1:
-                table.setWidget(i, 1, resolveButton);
-                break;
-            case 2:
-                writeLabel1.setEnabled(false);
-                writeLabel1.setGrid("s12");
-                if (txtAreaAdd.getText().isEmpty()) {
-                    writeLabel1.setPlaceholder("Write here:");
-                } else {
-                    writeLabel1.setPlaceholder(txtAreaAdd.getText());
-                }
-                table.setWidget(i, 1, writeLabel1);
-                break;
-            case 3:
-                writeLabel1.setEnabled(false);
-                writeLabel1.setGrid("s12");
-                if (txtAreaAdd.getText().isEmpty()) {
-                    writeLabel1.setPlaceholder("Write here:");
-                } else {
-                    writeLabel1.setPlaceholder(txtAreaAdd.getText());
-                }
-                writeLabel2.setEnabled(false);
-                writeLabel2.setGrid("s12");
-                writeLabel2.setPlaceholder("Write here:");
-                table.setWidget(i, 1, writeLabel1);
-                table.setWidget(i, 2, writeLabel2);
-                break;
-            default:
-                break;
-        }
 
-        window.add(table);
 
-    }
+
 
     /**
      * Combo box with the name of the existing widgets.
