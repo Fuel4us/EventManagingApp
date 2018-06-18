4cc0a86a3786578ea07ad8b387f0c8440c9edefa
Pedro Rodrigues (1140572) <1140572@isep.ipp.pt>
2018-06-18 18:40:37 +0100

    #14 [Implementation] TasksRepository and JpaTasksRepository created.

---
+++ server/src/main/java/pt/isep/nsheets/server/lapr4/green/s3/core/n1140572/tasks/persistence/TasksRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.isep.nsheets.server.lapr4.green.s3.core.n1140572.tasks.persistence;
+
+import eapli.framework.persistence.repositories.Repository;
+import pt.isep.nsheets.server.lapr4.green.s3.core.n1140572.tasks.domain.Tasks;
+
+/**
+ *
+ * @author Pedro Rodrigues - (1140572)
+ */
+public interface TasksRepository extends Repository<Tasks, Long>{
+    
+}
